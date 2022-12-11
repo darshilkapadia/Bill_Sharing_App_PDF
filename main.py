1c63211033b49f750e4ac747cf3eeddcf57f9170
darshilkapadia <97408199+darshilkapadia@users.noreply.github.com>
2022-12-11 00:56:42 -0500

    Updated

--- main.py
+++ main.py
@@ -76,14 +76,26 @@ class PdfReport:
         # to open file automatically (For mac users)
         webbrowser.open('file://' + os.path.realpath(self.filename))
 
-the_bill = Bill(amount=120, period = "April 2021")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
 
-print("John pays:", john.pays(bill=the_bill, flatmate2=marry))
-print("Marry pays:", marry.pays(bill=the_bill, flatmate2=john))
+# for cli interface
+amt = float(input("Hey, Enter the bill amount: "))
+period = input("What is Bill Period(e.g: December 2022): ")
+
+f_name1 = input("What is first Flatmate name?: ")
+days_fname1 = int(input(f"How many days did {f_name1} stays?: "))
+
+f_name2 = input("What is Second Flatmate name?: ")
+days_fname2 = int(input(f"How many days did {f_name2} stays?: "))
+
+
+the_bill = Bill(amount=amt, period = period)
+flatmate1 = Flatmate(name=f_name1, days_in_house=days_fname1)
+flatmate2 = Flatmate(name=f_name2, days_in_house=days_fname2)
+
+print(f"{flatmate1.name} pays:", flatmate1.pays(bill=the_bill, flatmate2=flatmate2))
+print(f"{flatmate2.name} pays:", flatmate2.pays(bill=the_bill, flatmate2=flatmate1))
 
 # to call the pdf generate method
 pdf_report = PdfReport(filename="Report-1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+pdf_report.generate(flatmate1=flatmate1, flatmate2=flatmate2, bill=the_bill)
 
